fb3dcc856fac3d855e7b0b4ef819015a5864bada
Krapht <kraphteu@gmail.com>
2024-07-11 20:21:06 +0200

    Reorganized menues

--- InsightLogParser.Client/Menu/AdvancedMenu.cs
+++ InsightLogParser.Client/Menu/AdvancedMenu.cs
@@ -4,17 +4,13 @@ namespace InsightLogParser.Client.Menu;
 
 internal class AdvancedMenu : IMenu
 {
-    private readonly MenuHandler _menuHandler;
     private readonly UserComputer _computer;
     private readonly MessageWriter _writer;
-    private readonly Spider _spider;
 
-    public AdvancedMenu(MenuHandler menuHandler, UserComputer computer, MessageWriter writer, Spider spider)
+    public AdvancedMenu(UserComputer computer, MessageWriter writer)
     {
-        _menuHandler = menuHandler;
         _computer = computer;
         _writer = writer;
-        _spider = spider;
     }
 
     public IEnumerable<(char? key, string text)> MenuOptions
@@ -22,14 +18,7 @@ internal class AdvancedMenu : IMenu
         get
         {
             yield return (null, "Advanced or risky operations. Press [h] to display this menu again and backspace to back");
-            if (_spider.IsOnline())
-            {
-                yield return ('w', "Log-in to cetus web ui");
-            }
             yield return ('c', "Cleanup steam screenshot.vdf file (at your own risk)");
-            yield return ('t', "Target puzzle by id");
-            yield return ('f', "List 15 closest (in 2d) puzzles to last teleport");
-            yield return ('F', "List 15 closest (in 3d) puzzles to last teleport");
         }
     }
 
@@ -37,33 +26,9 @@ internal class AdvancedMenu : IMenu
     {
         switch (keyChar)
         {
-            case 'w':
-                await _spider.OpenCetusWebAsync().ConfigureAwait(ConfigureAwaitOptions.None);
-                return MenuResult.Ok;
-
             case 'c':
                 ConfirmSteamScreenshotCleanup();
                 return MenuResult.PrintMenu;
-
-            case 't':
-                Console.Write("Puzzle Id: ");
-                var consoleString = Console.ReadLine();
-                if (!int.TryParse(consoleString, out var puzzleId))
-                {
-                    _writer.WriteError("Could not understand puzzle id");
-                    return MenuResult.Ok;
-                }
-                _spider.TargetPuzzle(puzzleId);
-                return MenuResult.Ok;
-
-            case 'f':
-                await _spider.ListClosestAsync(15, true);
-                return MenuResult.Ok;
-
-            case 'F':
-                await _spider.ListClosestAsync(15, false);
-                return MenuResult.Ok;
-
             default:
                 return MenuResult.NotValidOption;
         }
--- InsightLogParser.Client/Menu/CheeseMenu.cs
+++ InsightLogParser.Client/Menu/CheeseMenu.cs
@@ -5,10 +5,12 @@ namespace InsightLogParser.Client.Menu;
 internal class CheeseMenu : IMenu
 {
     private readonly Spider _spider;
+    private readonly MessageWriter _writer;
 
-    public CheeseMenu(Spider spider)
+    public CheeseMenu(Spider spider, MessageWriter writer)
     {
         _spider = spider;
+        _writer = writer;
     }
 
     public IEnumerable<(char? key, string text)> MenuOptions
@@ -20,6 +22,10 @@ internal class CheeseMenu : IMenu
             {
                 yield return ('s', "Open solution to last opened puzzle, if available");
                 yield return ('m', "Find mate to matchbox closest to last teleport");
+                yield return ('t', "Target puzzle by id");
+                yield return ('f', "List 15 closest (in 2d) puzzles to last teleport");
+                yield return ('F', "List 15 closest (in 3d) puzzles to last teleport");
+
             }
         }
     }
@@ -33,6 +39,24 @@ internal class CheeseMenu : IMenu
                 return MenuResult.Ok;
             case 'm':
                 _spider.TargetOtherMatchbox();
+                return MenuResult.Ok;
+            case 't':
+                Console.Write("Puzzle Id: ");
+                var consoleString = Console.ReadLine();
+                if (!int.TryParse(consoleString, out var puzzleId))
+                {
+                    _writer.WriteError("Could not understand puzzle id");
+                    return MenuResult.Ok;
+                }
+                _spider.TargetPuzzle(puzzleId);
+                return MenuResult.Ok;
+
+            case 'f':
+                await _spider.ListClosestAsync(15, true);
+                return MenuResult.Ok;
+
+            case 'F':
+                await _spider.ListClosestAsync(15, false);
                 return MenuResult.Ok;
             default:
                 return MenuResult.NotValidOption;
